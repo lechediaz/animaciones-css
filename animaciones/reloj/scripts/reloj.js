d2cd151e2ce5755cf8719ff543f13ec04b192479
David Fortaleché <lechediaz@gmail.com>
2020-01-16 22:45:20 -0500

    Mejora ocultar los dígitos del 4 al 9 en decenas de hora desde las 20 horas hasta las 0 horas

--- animaciones/reloj/scripts/reloj.js
+++ animaciones/reloj/scripts/reloj.js
@@ -1,66 +1,97 @@
+const reloj = (() => {
+    let horas_veinte = false;
 
-const crearCard = (inicio, fin, limite) => {
-    let html = '';
+    const llenarRueda = (rueda, limite) => {
+        let rueda_tag = document.getElementById(rueda);
+        let html = '';
 
-    for (let i = inicio; i < fin; i++) {
-        html += `<div class="card c${i}">${i <= limite ? i : ''}</div>`
+        for (let i = 0; i < 10; i++) {
+            html += `<div class="card c${i}">${i <= limite ? i : ''}</div>`
+        }
+
+        rueda_tag.innerHTML = html;
+    };
+
+    const rotarRueda = (rueda, digito) => {
+        let rueda_tag = document.getElementById(rueda);
+        let angulo = digito * 36;
+        const angulo_actual = parseInt(rueda_tag.dataset.angulo_actual || 0);
+        const rotaciones = Math.floor(angulo_actual / 360);
+
+        // console.log({
+        //     digito,
+        //     angulo,
+        //     angulo_actual,
+        //     rotaciones
+        // })
+
+        if ((angulo_actual - 360 * rotaciones) !== angulo) {
+            const nuevo_angulo = angulo === 0 ? 360 * (rotaciones + 1) : (360 * rotaciones + angulo);
+            rueda_tag.style.transform = `rotateX(-${nuevo_angulo}deg)`;
+            rueda_tag.dataset.angulo_actual = nuevo_angulo;
+        }
     }
 
-    return html;
-};
+    const mostrarCards = (rueda, inicio, fin, mostrar) => {
+        const rueda_tag = document.getElementById(rueda);
 
-const llenarRueda = (rueda, inicio, limite) => {
-    let rueda_tag = document.getElementById(rueda);
-    let html = crearCard(inicio, 10, limite) + crearCard(0, inicio, limite);
-
-    rueda_tag.innerHTML = html;
-};
-
-const crearReloj = () => {
-    const fecha_actual = new Date();
-    let hours_1 = Math.floor(fecha_actual.getHours() / 10);
-    let hours_2 = fecha_actual.getHours() - (hours_1 * 10);
-    let minutes_1 = Math.floor(fecha_actual.getMinutes() / 10);
-    let minutes_2 = fecha_actual.getMinutes() - (minutes_1 * 10);
-    let seconds_1 = Math.floor(fecha_actual.getSeconds() / 10);
-    let seconds_2 = fecha_actual.getSeconds() - (seconds_1 * 10);
-
-    llenarRueda('hours1', hours_1, 2);
-    llenarRueda('hours2', hours_2, 9);
-    llenarRueda('minutes1', minutes_1, 5);
-    llenarRueda('minutes2', minutes_2, 9);
-    llenarRueda('seconds1', seconds_1, 5);
-    llenarRueda('seconds2', seconds_2, 9);
-
-    horas_veinte = fecha_actual.getHours() >= 20;
-}
-
-const mostrarCards = (rueda, inicio, fin, mostrar) => {
-    fin++;
-    const rueda_tag = document.getElementById(rueda);
-
-    for (let i = inicio; i < fin; i++) {
-        rueda_tag.getElementsByClassName(`c${i}`)[0].textContent = mostrar ? i : '';
+        for (let i = inicio; i <= fin; i++) {
+            rueda_tag.getElementsByClassName(`c${i}`)[0].textContent = mostrar ? i : '';
+        }
     }
-}
 
-// Ejecución
-let horas_veinte = false;
+    const tickTack = () => {
+        const fecha_actual = new Date();
+        let hours_1 = Math.floor(fecha_actual.getHours() / 10);
+        let hours_2 = fecha_actual.getHours() % 10;
+        let minutes_1 = Math.floor(fecha_actual.getMinutes() / 10);
+        let minutes_2 = fecha_actual.getMinutes() % 10;
+        let seconds_1 = Math.floor(fecha_actual.getSeconds() / 10);
+        let seconds_2 = fecha_actual.getSeconds() % 10;
 
-crearReloj();
+        // Ocultar dígitos del 4 al 9 en decenas de hora desde las 20 horas hasta las 0 horas
+        if (fecha_actual.getHours() >= 20) {
+            if (!horas_veinte) {
+                mostrarCards('hours2', 4, 9, false);
+            }
 
-setInterval(() => {
-    const fecha_actual = new Date();
-
-    if (fecha_actual.getHours() >= 20) {
-        if (!horas_veinte) {
             horas_veinte = true;
-            mostrarCards('hours2', 4, 9, false);
-        }
-    } else {
-        if (horas_veinte) {
+        } else {
+            if (horas_veinte) {
+                mostrarCards('hours2', 4, 9, true);
+            }
+
             horas_veinte = false;
-            mostrarCards('hours2', 4, 9, true);
         }
+
+        // console.log({
+        //     hours_1,
+        //     hours_2,
+        //     minutes_1,
+        //     minutes_2,
+        //     seconds_1,
+        //     seconds_2
+        // });
+
+        rotarRueda('hours1', hours_1);
+        rotarRueda('hours2', hours_2);
+        rotarRueda('minutes1', minutes_1);
+        rotarRueda('minutes2', minutes_2);
+        rotarRueda('seconds1', seconds_1);
+        rotarRueda('seconds2', seconds_2);
     }
-}, 1000);
+
+    const crearReloj = () => {
+        llenarRueda('hours1', 2);
+        llenarRueda('hours2', 9);
+        llenarRueda('minutes1', 5);
+        llenarRueda('minutes2', 9);
+        llenarRueda('seconds1', 5);
+        llenarRueda('seconds2', 9);
+
+        tickTack();
+    }
+
+    crearReloj();
+    setInterval(tickTack, 1000);
+})();
